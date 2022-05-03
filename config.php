e4a5f745428147d822551be925d75ac16703e38b
bhuvi28 <100954043+bhuvi28@users.noreply.github.com>
2022-05-03 16:59:20 +0530

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('SENDGRID_API_KEY','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');
-    define('FROM_EMAIL','gameporium28@gmail.com');
-    define('FROM_NAME','GamePorium');
-?>
-
-     
